7ea2680c7310f793591b2a5bac4c6385d4de722b
firehawk81 <dto1981@hotmail.com>
2024-11-20 16:23:15 +0100

    initial commit

--- HotelWebApi_v2_Beta_v4/HotelWebApi_v2/verifications/budpay.aspx.cs
+++ HotelWebApi_v2_Beta_v4/HotelWebApi_v2/verifications/budpay.aspx.cs
@@ -24,8 +24,6 @@ namespace HotelWebApi_v2.verifications
                 if (verr.status == "true") {
                     pRefNo.InnerText = _refNo.ToUpper();
                     //Response.Redirect("www.google.com",false);
-
-                    
                 }
             }
         }
